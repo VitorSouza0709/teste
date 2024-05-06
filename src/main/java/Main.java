5e3b3febf545249a4473ee2121c72865a9a411e9
Vitor Moraes <33862548-vitorsouzamorae@users.noreply.replit.com>
2024-05-06 15:18:13 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("olá mundo !");
+    System.out.println("vitor souza");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
